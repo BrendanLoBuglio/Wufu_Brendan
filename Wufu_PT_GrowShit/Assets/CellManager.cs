996fcc619f0f0bc99150093e4a84f53d2bef1512
Brendan LoBuglio <B.LoBuglio@gmail.com>
2014-08-14 17:48:50 -0400

    First Commit, yo

---
+++ Wufu_PT_GrowShit/Assets/CellManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CellManager : MonoBehaviour {
+	public GameObject cellPrefab;
+	public int cellGridColumns = 10;
+	public int cellGridRows = 10;
+	public float cellAlpha = 0.1f;
+	
+	public GameObject[,] cells;
+	
+	
+	private Vector3 testVector;
+	private float cellWidth;
+	void Start () 
+	{
+		cellWidth = (cellPrefab.renderer.bounds.max.x - cellPrefab.collider.bounds.min.x) * 2f;		
+		InstantiateCells(cellGridColumns, cellGridRows);
+	}
+	
+	void Update () {
+		testVector = cellPrefab.collider.bounds.size;//cellPrefab.collider.bounds.extents;
+	}
+	
+	void InstantiateCells(int rows, int columns)
+	{
+		cells = new GameObject[rows,columns];
+		
+		Vector3 startingPoint = transform.position - new Vector3(0.5f*rows*cellWidth,0 , 0.5f*columns*cellWidth);
+		for(int x = 0; x < columns; x++)
+		{
+			for(int y = 0; y < rows; y++)
+			{
+				Vector3 nextCellPosition = new Vector3(startingPoint.x+x*cellWidth, startingPoint.y, startingPoint.z+y*cellWidth);
+				cells[x,y] = (GameObject)Instantiate(cellPrefab, nextCellPosition, Quaternion.identity);
+				cells[x,y].renderer.material.color = new Color(Random.Range(0f,1f),Random.Range(0f,1f),Random.Range(0f,1f), cellAlpha);
+			}
+		}
+	}
+}
---
+++ Wufu_PT_GrowShit/Assets/LifeCell.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LifeCell : MonoBehaviour {
+	
+	public GameObject emptyFab;
+	public GameObject grassFab;
+	public GameObject flowerFab;
+	public GameObject bushFab;
+	public GameObject saplingFab;
+	public GameObject treeFab;
+	
+	public GameObject containedPlant;//ContainedPlant holds the piece of plant life currently living in this cell
+	public GameObject containedGrass;//ContainedPlant holds the unit of grass currently living in this cell. This is stored separate from ContainedPlant so that the grass remains even as further growth occurs.
+	
+	public float growthLevel = 0;/*
+	{
+		get
+		{
+			return growthLevel;
+		}
+		set
+		{
+			growthLevel = value;
+			ManageContainedPlant();
+		}
+	}*/
+	private float oldGrowthLevel = 0;
+	
+	void Start()
+	{
+		containedPlant = (GameObject)Instantiate(emptyFab, transform.position, Quaternion.identity);
+		containedGrass = (GameObject)Instantiate(emptyFab, transform.position, Quaternion.identity);
+	}
+	void Update()
+	{
+		growthLevel = Mathf.Clamp(growthLevel, 0f, 1f);
+		if(oldGrowthLevel != growthLevel){
+			ManageContainedPlants();
+		}
+		oldGrowthLevel = growthLevel;
+	}
+	
+	void ManageContainedPlants () 
+	{
+		if(growthLevel < 0.05 && !containedGrass.GetComponent<EmptyGameObject>()){
+			Destroy(containedGrass);
+			containedGrass = (GameObject)Instantiate(emptyFab, transform.position, Quaternion.identity);
+		}
+		if(growthLevel >= 0.05 && growthLevel < 0.4 && (!containedGrass.GetComponent<Grass>() || !containedPlant.GetComponent<EmptyGameObject>())){
+			Destroy(containedPlant);
+			containedPlant = (GameObject)Instantiate(emptyFab, transform.position, Quaternion.identity);
+			Destroy(containedGrass);
+			containedGrass = (GameObject)Instantiate(grassFab, transform.position, Quaternion.identity);
+		}
+		if(growthLevel >= 0.4 && growthLevel < 0.6 && !containedPlant.GetComponent<Flower>()){
+			Destroy(containedPlant);
+			containedPlant = (GameObject)Instantiate(flowerFab, transform.position, Quaternion.identity);
+		}
+		if(growthLevel >= 0.6 && growthLevel < 0.8 && !containedPlant.GetComponent<Bush>()){
+			Destroy(containedPlant);
+			containedPlant = (GameObject)Instantiate(bushFab, transform.position, Quaternion.identity);
+		}
+		if(growthLevel >= 0.8 && growthLevel < 1 && !containedPlant.GetComponent<Sapling>()){
+			Destroy(containedPlant);
+			containedPlant = (GameObject)Instantiate(saplingFab, transform.position, Quaternion.identity);
+		}
+		if(growthLevel >= 1 && !containedPlant.GetComponent<Oak>()){
+			Destroy(containedPlant);
+			containedPlant = (GameObject)Instantiate(treeFab, transform.position, Quaternion.identity);
+		}
+	}
+}
